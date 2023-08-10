2ff1ef8651a7796724ac5b0f9cf60b603c073daf
osamakhanedu <osamakhanedu@gmail.com>
2023-08-10 16:56:10 +0500

    handling parametize url

--- http-server/index.js
+++ http-server/index.js
@@ -4,13 +4,24 @@ const server = http.createServer();
 
 
 server.on('request',(req, res) => {
-    if (req.url === "/friends") {
+    const items = req.url.split("/");
+
+    const list = [{id:0, name: "Apple"}, {id:1, name:"Bat"}, {id:2, name: "Cat"}];
+
+    if ( items[1] === "friends") {
         res.writeHead(200, {
             "Content-Type": "application/json",
         });
 
-        res.end(JSON.stringify({ id: 1, name: "ABC apple" }));
-    } else if (req.url === "/messages") {
+        if(items.length === 3){
+            return res.end(JSON.stringify(list[items[2]]));
+
+        }else{
+            return res.end(JSON.stringify(list));
+
+        }
+
+    } else if (items[1] === "messages") {
 
         res.setHeader('Content-type','text/html')
 
@@ -19,9 +30,9 @@ server.on('request',(req, res) => {
         res.write("<ul>");
         res.write("<li> Hello Osama</li>");
 
-        res.write("</html>");
-        res.write("</body>");
         res.write("</ul>");
+        res.write("</body>");
+        res.write("</html>");
         res.end();
     }else{
         res.statusCode = 404;
